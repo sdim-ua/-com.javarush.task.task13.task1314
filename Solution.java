f435866da45433d1783fed73f3831ad07b99f9aa
sdim-ua <ingersol@yandex.ua>
2019-06-24 10:04:19 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+
+/* 
+Большая лисица — это такая лисица
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Fox bigFox = new BigFox();
+        System.out.println(bigFox.getName());
+        System.out.println(bigFox.getColor());
+
+    }
+
+    public interface Animal {
+       Color getColor();
+    }
+
+    public static abstract class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+         public Color getColor(){
+              return Color.GRAY;
+         }
+    }
+
+    public static class BigFox extends Fox {
+        public Color getColor() {
+            return Color.GRAY;
+        }
+    }
+
+}
